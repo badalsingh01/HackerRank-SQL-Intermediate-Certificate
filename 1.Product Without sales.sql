94468ea9f37e25b133c8e7ce6026ee2dc84941f0
Badal Chaudahry <99376974+badalsingh01@users.noreply.github.com>
2023-07-30 22:16:03 +0530

    Update 1.Product Without sales.sql

--- 1.Product Without sales.sql
+++ 1.Product Without sales.sql
@@ -1,1 +1,7 @@
-
+--Product Without sales Solution
+  /*
+  Product Without sales Solution
+*/
+SELECT product.sku, product.product_name
+FROM product
+WHERE product.id NOT IN (SELECT product_id FROM invoice_item);
